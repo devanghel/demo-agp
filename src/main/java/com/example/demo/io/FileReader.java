2c84f5f9649d62399a39f0c2e024b55d35e0d77c
Alexandru Anghel <tldr@Alexandrus-MacBook-Air.local>
2021-06-03 15:58:01 +0300

    WIP

--- src/main/java/com/example/demo/io/FileReader.java
+++ src/main/java/com/example/demo/io/FileReader.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public interface FileReader {
 
+    // TODO: 03/06/2021 think or research if it is posible to generify List and Object to make a single method read that will return both
     <T> List<T> readArray();
 
     <T> T readObject();
--- src/main/java/com/example/demo/io/JsonFileReader.java
+++ src/main/java/com/example/demo/io/JsonFileReader.java
@@ -20,29 +20,39 @@ public class JsonFileReader implements FileReader {
     private String filePath;
 
     public void read(boolean isArray) {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
+        if(isArray()) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
 
-            // TODO: 03/06/2021 think of a more generic way to parse any valid json structure be it array or obj
-            Map<?, ?> map = mapper.readValue(Paths.get(filePath).toFile(), Map.class);
+                // TODO: 03/06/2021 think of a more generic way to parse any valid json structure be it array or obj
+                Map<?, ?> map = mapper.readValue(Paths.get(filePath).toFile(), Map.class);
 
-            for (Map.Entry<?, ?> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + "=" + entry.getValue());
+                for (Map.Entry<?, ?> entry : map.entrySet()) {
+                    System.out.println(entry.getKey() + "=" + entry.getValue());
+                }
+            }catch (IOException ioException) {
+                // TODO: 03/06/2021 if profile dev, print stacktrace
+                log.error(ioException.getMessage());
             }
-        }catch (IOException ioException) {
-            // TODO: 03/06/2021 if profile dev, print stacktrace
-            log.error(ioException.getMessage());
         }
     }
 
     @Override
     public <T> List<T> readArray() {
-        return read();
-    }
+       if(isArray()) {
 
+       }else {
+           throw new ObjectTypeDidNotMatchException(OBJECT);
+
+    }
+    //todo evaluate the idea of dinamycly reading and pusshing every file, and after the push you would save the file in the arcFolder,also expose it back to the filesystem in docker
     @Override
     public <T> T readObject() {
-        return read(isArray());
+           if(!isArray()){
+
+           }else {
+               throw new ObjectTypeDidNotMatchException(ARRAY);
+           }
     }
 
     @Override
