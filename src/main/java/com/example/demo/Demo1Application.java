72a94b9b6342fa26965e82739496553ace3e02d6
Alexandru Anghel <tldr@Alexandrus-MacBook-Air.local>
2021-06-03 22:00:12 +0300

    docker compose for rabbit, and a litle refactoring of the main class

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -1,17 +1,16 @@
 package com.example.demo;
 
 
-import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@AllArgsConstructor
-public class Demo1Application extends SpringBootServletInitializer {
+@NoArgsConstructor
+public class Demo1Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringApplication.class, args).close();
+    public static void main(String[] argsSpringApplication) {
+        SpringApplication.run(Demo1Application.class, argsSpringApplication).close();
     }
 
 }
