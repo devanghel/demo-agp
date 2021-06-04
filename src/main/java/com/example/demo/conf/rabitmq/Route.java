5427fef96829f495dee1a88053e70e42f378e895
Alexandru Anghel <tldr@Alexandrus-MacBook-Air.local>
2021-06-05 00:27:23 +0300

    WIP, project running

--- src/main/java/com/example/demo/conf/rabitmq/Route.java
+++ src/main/java/com/example/demo/conf/rabitmq/Route.java
@@ -1,25 +1,25 @@
 package com.example.demo.conf.rabitmq;
 
-public class Route extends RouteBuilder {
+public class Route { //extends RouteBuilder {
 
-    @Override
-    public void configure() {
-
-        from("timer:foo?period=100000")
-                .routeId("NEW YEAR ROUTE")
-                .log(INFO, this.log, "Hello!")
-                .delay(2000)
-                .log(INFO, this.log, "Just wanted to wish you good people Happy New Year!")
-                .delay(2000)
-                .log(INFO, this.log, "Stay safe.")
-                .delay(2000)
-                .log(INFO, this.log, "Enjoy life.")
-                .delay(2000)
-                .log(INFO, this.log, "Learn a lot.")
-                .delay(2000)
-                .log(INFO, this.log, "Love even more")
-                .delay(2000)
-                .log(INFO, this.log, "Cheers!")
-                .log(INFO, this.log, "George / Djordje");
+//    @Override
+//    public void configure() {
+//
+//        from("timer:foo?period=100000")
+//                .routeId("NEW YEAR ROUTE")
+//                .log(INFO, this.log, "Hello!")
+//                .delay(2000)
+//                .log(INFO, this.log, "Just wanted to wish you good people Happy New Year!")
+//                .delay(2000)
+//                .log(INFO, this.log, "Stay safe.")
+//                .delay(2000)
+//                .log(INFO, this.log, "Enjoy life.")
+//                .delay(2000)
+//                .log(INFO, this.log, "Learn a lot.")
+//                .delay(2000)
+//                .log(INFO, this.log, "Love even more")
+//                .delay(2000)
+//                .log(INFO, this.log, "Cheers!")
+//                .log(INFO, this.log, "George / Djordje");
 
     }
