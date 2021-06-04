c9e015ad49fdede5f1b886226ee59d6d49376390
Alexandru Anghel <tldr@Alexandrus-MacBook-Air.local>
2021-06-05 00:25:36 +0300

    Added apache camel rabbit and route blueprint

--- src/main/java/com/example/demo/rabbit/Receiver.java
+++ src/main/java/com/example/demo/rabbit/Receiver.java
@@ -7,16 +7,8 @@ import java.util.concurrent.CountDownLatch;
 @Component
 public class Receiver {
 
-    private CountDownLatch latch = new CountDownLatch(1);
-
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
-        latch.countDown();
     }
-
-    public CountDownLatch getLatch() {
-        return latch;
-    }
-
 }
 
