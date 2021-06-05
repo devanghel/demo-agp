b47567ef41ce6ad9ee2e5a13b00c5a90b12f91c3
Alexandru Anghel <tldr@Alexandrus-MacBook-Air.local>
2021-06-05 06:26:05 +0300

    WIP, arch

--- src/main/java/com/example/demo/conf/rabitmq/routes/RabbitMqProducerRoute.java
+++ src/main/java/com/example/demo/conf/rabitmq/routes/RabbitMqProducerRoute.java
@@ -1,55 +1,16 @@
 package com.example.demo.conf.rabitmq.routes;
 
-import lombok.Builder;
-import org.apache.camel.builder.RouteBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.stereotype.Service;
+import com.example.demo.CustomRouterBuilder;
 
+public final class RabbitMqProducerRoute extends CustomRouterBuilder {
 
-public class RabbitMqProducerRoute extends RouteBuilder {
-
-    public static RabbitMqProducerRoute withBody(String body) {
-        RabbitMqProducerRoute rabbitMqProducerRoute = new RabbitMqProducerRoute();
-        this.body = body;
-        return rabbitMqProducerRoute;
-    }
-    /*
-    I prefer to not use default values as I think it's a security concern once someone cracks the obfuscation
-     */
-    @Value("${file.update.period}")
-    private long fileUpdatePeriod;
-
-    @Value("${tlas}")
-    private String tlas;
-
-    @Value("${drsap}")
-    private String drsap;
-
-    private String body;
-
-    public String getTlas() {
-        throw new RuntimeException();
-    }
-
-    public String getBody() {
-        throw new RuntimeException();
-    }
-
-    public long getFileUpdatePeriod() {
-        throw new RuntimeException();
+    @Override
+    protected void setBody(String body) {
+        super.setBody(body);
     }
 
     @Override
-    public void configure() {
-        from("timer:foo?period=" + fileUpdatePeriod)
-                .routeId("RabbitMqProducer")
-                .setBody().constant(encrypt(body))
-                .onCompletion().log("was sent succesefully");
-                // TODO: 05/06/2021 add error and debug to the log only in debug mode, prod will go to kibana
-    }
-
-    private String encrypt(String toBeEncrypted) {
-        return Encryptors.text(drsap, tlas).encrypt(toBeEncrypted);
+    public void setRouteId(String routeId) {
+        super.setRouteId(routeId);
     }
 }
