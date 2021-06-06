36ac30914003fb362a393df4c32423d87f52ea8d
Alexandru Anghel <tldr@Alexandrus-MacBook-Air.local>
2021-06-06 22:49:13 +0300

    added new todo

--- src/main/java/com/example/demo/model/bid/Bid.java
+++ src/main/java/com/example/demo/model/bid/Bid.java
@@ -12,6 +12,7 @@ public class Bid {
     private String pl;
 
 //    Log the bid id(id), timestamp(ts), queue “name”( ty), and payload (decoded from Base64 pl) when the queueing is completed.
+// TODO: 06/06/2021 use placeholders for string concatenation
     @Override
     public String toString() {
         return "Bid{" +
